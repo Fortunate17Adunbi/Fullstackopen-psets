d69c2ec84a677b292a54bb3aebf222e2ff694b7f
Fortunate <samuelfortunate098@gmail.com>
2025-09-07 04:19:18 +0100

    Convert phonebook_backend from submodule to regular folder

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,141 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/phonebook')
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(requestLogger)
+// app.use(morgan(':request'))
+app.use(morgan(':method :url - :status :res[content-length] - :response-time ms :request'))
+
+morgan.token('request', function (request, ) {
+  return JSON.stringify(
+    request.body
+  )
+})
+// const generateId = () => {
+//   const maxId = persons.length > 0 ?
+//     Math.max(...persons.map(person => Number(person.id))) :
+//     0
+
+//   return String(maxId + 1)
+// }
+
+app.get('/', (request, response) => {
+  response.send('<h1>Welcome to Phone book</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(person => {
+    response.json(person)
+  }).catch(error => {
+    console.log(`Could not get numbers: ${error}`)
+  })
+})
+
+app.get('/info', (request, response, next) => {
+  Person.find({}).then(person => {
+    response.send(`
+        <p>Phone book has info for ${person.length} people</p>
+        <br />
+        <p>${new Date()}</p>
+    `)
+  }).catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  Person.findById(id).then(result => {
+    response.json(result)
+  }).catch(error => {
+    console.log(`Could not get perons ${error.message}`)
+  })
+})
+
+app.post('/api/persons/', (request, response, next) => {
+  const body = request.body
+
+  if ((!body.name) || (!body.number)) {
+    return response.status(400).json({
+      error: 'Missing name or number'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  Person.find({ name: person.name }).then(result => {
+    if (result.length > 0) {
+      console.log('name already exist')
+      return response.status(401).json({
+        error: 'Name already exists'
+      })
+    }
+    // else if (result.length === 0) {
+    //   console.log('does not exist')
+    //   return response.status(404).json({
+    //     error: `information of ${person.name} has already been removed from server`
+    //   })
+    // }
+
+    person.save().then(result => {
+      console.log(`Added ${person.name} number ${person.number} to phonebook`)
+      response.json(result)
+    }).catch(error => next(error))
+  })
+
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+    .then(updatedNumber => {
+      response.json(updatedNumber)
+    }).catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id).then(deletedNumber => {
+    console.log('deleted ', deletedNumber)
+    if (!deletedNumber) {
+      return response.status(404).end()
+    }
+    response.json(deletedNumber)
+    response.status(204).end()
+  }).catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
---
+++ part3/phonebook_backend/models/phonebook.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URL
+
+console.log('connecting to mongodb: ', url)
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to mongodb')
+  })
+  .catch(error => {
+    console.log(`Error connecting to mongodb: ${error.message}`)
+  })
+
+const phoneRegExp = /^(\d{2}|\d{3})-(\d{6,})$/
+const phonebookSchema = new mongoose.Schema({
+  name: {
+    minLength: 3,
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return phoneRegExp.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`,
+    },
+    required: true,
+  },
+})
+
+phonebookSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+module.exports = mongoose.model('Person', phonebookSchema)
