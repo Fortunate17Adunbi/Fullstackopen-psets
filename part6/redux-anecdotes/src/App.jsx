3014f1d4c187e0e24ae0dd418084367d69666aba
Fortunate <samuelfortunate098@gmail.com>
2025-09-17 14:31:53 +0100

    completed part6a

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,16 @@
+import { createAnecdote, voteAnec } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
+
+const App = () => {
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
